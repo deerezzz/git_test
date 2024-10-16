0372990a552c702cdf224a247bd74ad246d4d4a6
DeeRezz <deerezzz86@gmail.com>
2024-10-16 15:04:00 -0700

    LC 2704

---
+++ .history/codeCademy/tutorials/tryCatch_20241016105149.js
@@ -0,0 +1,9 @@
+try {
+    throw Error('blah blah'); 
+  } catch (e) {
+    console.log(e); 
+  }
+  console.log('Throw error caught and catched')
+  
+  
+  
---
+++ .history/codeWars/reduceButGrow_20241016101519.js
@@ -0,0 +1,5 @@
+function grow(x){
+
+    return x.reduce((acc, num) => acc * num, 1);
+}
+
