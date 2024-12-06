03def359adafdfae285856f820c611abae4031ef
DeeRezz <deerezzz86@gmail.com>
2024-12-05 17:55:49 -0800

    changes made to game-hub, downgraded to chakra 2.0

--- game-hub/src/main.tsx
+++ game-hub/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes"; // Assuming you're using next-themes for dark mode
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// Define your custom theme here if necessary
+const theme = extendTheme({
+  // Custom theme configurations go here
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
