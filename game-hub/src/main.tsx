5118f1c0e16859fbb88b779df2d9d421232c4aaa
DeeRezz <deerezzz86@gmail.com>
2024-12-26 11:44:11 -0800

    implemented dark mode

--- game-hub/src/main.tsx
+++ game-hub/src/main.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
 import { ThemeProvider } from "next-themes"; // Assuming you're using next-themes for dark mode
 import App from "./App";
-
-// Define your custom theme here if necessary
-const theme = extendTheme({
-  // Custom theme configurations go here
-});
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
       <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
       </ChakraProvider>
     </ThemeProvider>
