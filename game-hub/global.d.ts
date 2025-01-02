40553ae534f319960f6e51a981f6e53699d0ddb7
DeeRezz <deerezzz86@gmail.com>
2025-01-02 15:33:24 -0800

    I got rid of game-hub due to the lack of access of an api key for the project.

--- game-hub/global.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace NodeJS {
-    interface ProcessEnv {
-      VITE_CLIENT_ID: string;
-      VITE_CLIENT_SECRET: string;
-    }
-  }
-  
--- game-hub/src/Compo/ColorModeSwitch.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
-const ColorModeSwitch = () => {
-    const { toggleColorMode, colorMode } = useColorMode();
-    return (_jsxs(HStack, { children: [_jsx(Switch, { colorScheme: "green", isChecked: colorMode === "dark", onChange: toggleColorMode }), _jsx(Text, { children: "Dark Mode" })] }));
-};
-export default ColorModeSwitch;
